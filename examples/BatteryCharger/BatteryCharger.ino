cff1548507a31a514fe16fa9e73e5ac633013bbf
Mehmet Günce Akkoyun <akkoyun@me.com>
2024-02-16 10:06:11 +0300

    Bug fix

--- examples/BatteryCharger/BatteryCharger.ino
+++ examples/BatteryCharger/BatteryCharger.ino
@@ -1,8 +1,10 @@
+// Include Library
 #include <BQ24298.h>
 
 // Create Charger Object
-BQ24298 Charger(true, __B108AA_MUX_Power__);
+BQ24298 Charger(true, 1);
 
+// Setup
 void setup() {
 
 	// Start Serial
@@ -13,54 +15,56 @@ void setup() {
 
 }
 
+// Loop
 void loop() {
 
 	// Print Diagnostic
-	Serial.print("PG Stat             : "); if (!Charger.PG_STAT()) {Serial.println("Power Not Good");} else {Serial.println("Power Good");};
-	Serial.print("Therm Stat          : "); if (Charger.THERM_STAT()) {Serial.println("Thermal Hot!"); } else {Serial.println("Normal");};
-	Serial.print("DPM Stat            : "); if (!Charger.DPM_STAT()) {Serial.println("DPM Pasive"); } else {Serial.println("DPM Active");};
-	Serial.print("WatchDog Fault      : "); if (Charger.WATCHDOG_FAULT()) {Serial.println("Watchdog expired"); } else {Serial.println("Normal");};
-	Serial.print("Battery OVP         : "); if (Charger.BAT_OVP_FAULT()) {Serial.println("Bat OVP"); } else {Serial.println("Normal");};
-	Serial.print("System Voltage Stat : "); if (Charger.VSYS_STAT()) {Serial.println("BAT < VSYSMIN"); } else {Serial.println("BAT > VSYSMIN");};
-	Serial.print("Bat LowV            : "); if (Charger.BATLOWV()) {Serial.println("3V0"); } else {Serial.println("2V8");};
-	Serial.print("Bat Cold            : "); if (Charger.BCOLD()) {Serial.println("BCold True"); } else {Serial.println("Normal");};
-	Serial.print("NTC Fault           : "); if (Charger.BCOLD()) {Serial.println("Fault"); } else {Serial.println("Normal");};
-	Serial.print("Input Voltage Stat  : "); 
+	Serial.print(F("PG Stat             : ")); if (!Charger.PG_STAT()) {Serial.println(F("Power Not Good"));} else {Serial.println(F("Power Good"));};
+	Serial.print(F("Therm Stat          : ")); if (Charger.THERM_STAT()) {Serial.println(F("Thermal Hot!")); } else {Serial.println(F("Normal"));};
+	Serial.print(F("DPM Stat            : ")); if (!Charger.DPM_STAT()) {Serial.println(F("DPM Pasive")); } else {Serial.println(F("DPM Active"));};
+	Serial.print(F("WatchDog Fault      : ")); if (Charger.WATCHDOG_FAULT()) {Serial.println(F("Watchdog expired")); } else {Serial.println(F("Normal"));};
+	Serial.print(F("Battery OVP         : ")); if (Charger.BAT_OVP_FAULT()) {Serial.println(F("Bat OVP")); } else {Serial.println(F("Normal"));};
+	Serial.print(F("System Voltage Stat : ")); if (Charger.VSYS_STAT()) {Serial.println(F("BAT < VSYSMIN")); } else {Serial.println(F("BAT > VSYSMIN"));};
+	Serial.print(F("Bat LowV            : ")); if (Charger.BATLOWV()) {Serial.println(F("3V0")); } else {Serial.println(F("2V8"));};
+	Serial.print(F("Bat Cold            : ")); if (Charger.BCOLD()) {Serial.println(F("BCold True")); } else {Serial.println(F("Normal"));};
+	Serial.print(F("NTC Fault           : ")); if (Charger.BCOLD()) {Serial.println(F("Fault")); } else {Serial.println(F("Normal"));};
+	Serial.print(F("Input Voltage Stat  : ")); 
 	switch (Charger.VBUS_STAT()) {
 	case 1:
-		Serial.println("USB");
+		Serial.println(F("USB"));
 		break;
 	case 2:
-		Serial.println("Adapter");
+		Serial.println(F("Adapter"));
 		break;
 	case 3:
-		Serial.println("OTG");
+		Serial.println(F("OTG"));
 		break;
 	default:
-		Serial.println("Unknown");
+		Serial.println(F("Unknown"));
 		break;
 	}
-	Serial.print("Charge Status       : "); 
+	Serial.print(F("Charge Status       : "));
 	switch (Charger.Charge_Status()) {
 	case 1:
-		Serial.println("Pre-Charge");
+		Serial.println(F("Pre-Charge"));
 		break;
 	case 2:
-		Serial.println("Fast Charge");
+		Serial.println(F("Fast Charge"));
 		break;
 	case 3:
-		Serial.println("Charge Complate");
+		Serial.println(F("Charge Complate"));
 		break;
 	default:
-		Serial.println("Not Charging");
+		Serial.println(F("Not Charging"));
 		break;
 	}
-	Serial.print("Fault register      : 0x"); Serial.print(Charger.Get_Fault_Register(), HEX); Serial.println("");
-	Serial.println("----------------------------------"); 
+	Serial.print(F("Fault register      : 0x")); Serial.print(Charger.Get_Fault_Register(), HEX); Serial.println("");
+	Serial.println(F("----------------------------------")); 
 
 	// Reset Watchdog
 	Charger.Reset_Watchdog();
 
+	// Delay
 	delay(3000);
 
 }
